648010e9deb2b3e24b9c060738d91b20ebcc2d10
kishlays008 <kishlays001@gmail.com>
2026-04-24 15:13:37 +0530

    Create deletion in a singly linkedlist.java

---
+++ Linkedlist/deletion in a singly linkedlist.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static Node deleteNode(Node head, int x) {
+        if (x == 1) {
+            return head.next;
+        }
+        Node temp = head;
+        for (int i = 1; i < x - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+}
